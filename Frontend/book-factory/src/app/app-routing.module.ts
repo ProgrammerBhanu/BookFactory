01ceff90e4fa0f67edbc96ec74a14f9e28f92211
Vinay Makade <91530880+Vinay2603@users.noreply.github.com>
2022-05-24 15:50:10 +0530

    add

--- Frontend/book-factory/src/app/app-routing.module.ts
+++ Frontend/book-factory/src/app/app-routing.module.ts
@@ -1,30 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AllBookComponent } from './all-book/all-book.component';
+import { CartPageComponent } from './cart-page/cart-page.component';
 import { HomeComponent } from './home/home.component';
 import { LoginRegisterComponent } from './login-register/login-register.component';
+import { PaymentPageComponent } from './payment-page/payment-page.component';
 
 const routes: Routes = [
   {
-    path:"",
-    component:HomeComponent
+    path: '',
+    component: HomeComponent,
   },
   {
-    path:"login",
-    component:LoginRegisterComponent
+    path: 'login',
+    component: LoginRegisterComponent,
   },
   {
-    path:"register",
-    component:LoginRegisterComponent
+    path: 'register',
+    component: LoginRegisterComponent,
   },
   {
-    path:"allbooks",
-    component:AllBookComponent,
-  }
+    path: 'allbooks',
+    component: AllBookComponent,
+  },
+  {
+    path: 'cart',
+    component: CartPageComponent,
+  },
+  {
+    path: 'cart/payment',
+    component: PaymentPageComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
